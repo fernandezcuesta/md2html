7b6fc8abf8e1a99583243e929b5c732435dcfb77
Jesús Fernández <fernandez.cuesta@gmail.com>
2015-04-16 12:40:13 +0200

    Small refactoring, tests and layouts fixed. Now it is possible to set a base directory where /css and /layout hang.

--- md2html/md2html.py
+++ md2html/md2html.py
@@ -20,122 +20,11 @@ from urllib2 import urlopen, HTTPError, URLError
 from csscompressor import compress
 
 
-THIS_DIR = os.path.dirname(os.path.abspath(__file__))
-HTML_TEMPLATE = 'layout/template.html'
+HTML_TEMPLATE = 'template.html'
 MD_EXTENSIONS = ['codehilite', 'tables', "toc(marker='')", 'meta']
 
 
-def render_html(**kwargs):
-    """ Renders an html from jinja2 templates filled with content converted
-        from markdown source
-        Mandatory arguments:
-         - md_fileobject: markdown input file object (passed to md2html)
-
-        Optional arguments:
-
-        - html_template:  Template for jinja2
-        - output_file:    Writtable output file descriptor object
-        - css_files:      List of CSS styles to be applied to the HTML file
-        - favicon_file:   Optional favicon file
-        - background_img: Optional background image file
-        - md_extensions:  List of extensions for markdown module (for md2html)
-    """
-
-    html_template = kwargs.pop('html_template', HTML_TEMPLATE)
-    css_files = kwargs.pop('css_files', [])
-    output_file_descr = kwargs.pop('output_file', None)
-    favicon_file = kwargs.pop('favicon', None)
-    background_img = kwargs.pop('background_img', None)
-    logo_img = kwargs.pop('logo', None)
-
-    loader = jinja2.FileSystemLoader(THIS_DIR)
-    environment = jinja2.Environment(loader=loader,
-                                     trim_blocks=True)
-    environment.globals['include_raw'] = \
-        lambda filename: jinja2.Markup(loader.get_source(environment,
-                                                         filename)[0]) \
-        if os.path.exists(filename) else ''
-
-    environment.globals['gen_author_list'] = \
-        lambda authors, emails: [''] if len(authors) != len(emails) else \
-        [u'<a href="mailto:{}">{}</a>'.format(emails[i], author)
-         for (i, author) in enumerate(authors)]
-
-    environment.globals['gen_copyright_list'] = \
-        lambda copyright: [u'<a href={1}>{0}</a>'.format(*item.split(','))
-                           for item in copyright]
-
-    environment.globals['css_compress'] = \
-        lambda css_list: ''.join(css_compress(['css/%s' % css
-                                               for css in css_list]))
-
-    environment.globals['to_base64'] = \
-        lambda image: to_b64_image(image[0])
-
-    template = environment.get_template(html_template)
-    content, metadata, toc = md2html(**kwargs)
-
-    if 'logo' in metadata:  # input argument overrides markdown's metadata
-        logo_img = logo_img or metadata['logo'][0]
-
-    if 'favicon' in metadata:  # input argument overrides markdown's metadata
-        favicon_file = favicon_file or metadata['favicon'][0]
-
-    if 'background' in metadata:
-        background_img = background_img or metadata['background'][0]
-
-    markdown_date = ', '.join(metadata['date']) if 'date' in metadata \
-        else time.strptime('%d-%B-%Y')
-
-    fbuffer = StringIO()
-    fbuffer.write(template.render(css_lines=';'.join(css_compress(css_files)),
-                                  content=content,
-                                  metadata=metadata,
-                                  toc=toc,
-                                  favicon=to_b64_image(favicon_file),
-                                  logo=to_b64_image(logo_img),
-                                  background=to_b64_image(background_img),
-                                  date=markdown_date).encode('utf-8'))
-    fbuffer.seek(0)
-    if output_file_descr:
-        output_file_descr.write(reencode_html(fbuffer))
-    else:
-        print(reencode_html(fbuffer))
-    fbuffer.close()
-    return
-
-
-def reencode_html(input_html):
-    """
-    Embeds all images from an html file to base64
-    """
-    fbuffer = StringIO()
-    my_regex = re.compile('.*(<img.+src=\")(.+?)"', re.IGNORECASE)
-    for line in input_html:
-        try:
-            item = re.finditer(my_regex, line).next().group(2)
-            fbuffer.write(line.replace(item, to_b64_image(item)))
-        except StopIteration:
-            fbuffer.write(line)
-    fbuffer.seek(0)
-    content = fbuffer.read()
-    fbuffer.close()
-    return content
-
-
-def css_compress(css_files):
-    """ Reads file by file and returns the compressed version of the CSS
-    css_files: list of files
-    """
-    for item in css_files:
-        try:
-            with open('{}/{}'.format(THIS_DIR,item), 'r') as css_file:
-                yield compress(css_file.read())
-        except IOError:
-            yield ''
-
-
-def to_b64_image(image_filename):
+def to_b64(image_filename):
     """ Returns a tuple with (b64content, imgtype) where:
         - b64content is a base64 representation of the input file
         - imgtype is the image type as detected by imghdr
@@ -144,7 +33,7 @@ def to_b64_image(image_filename):
         img_info = parse(image_filename, rule='IRI')
         extension = img_info['path'].split('.')[-1]
         content = urlopen(image_filename)
-    except ValueError:  # image_filename is not a valid IRI, assume local file
+    except ValueError:  # not a valid IRI, assume local file
         try:
             extension = imghdr.what(image_filename)
             if extension is None:
@@ -160,34 +49,159 @@ def to_b64_image(image_filename):
     return txt
 
 
-def md2html(md_fileobject, md_extensions=None):
-    """ Converts markdown syntax to html
-         - md_fileobject: markdown input file object
-         - md_extensions [optional]: list of extensions for markdown module
-
-        Returns:
-        - html:    HTML encoded string
-        - md.Meta: Metadata as found in md header (if any)
-        - md.toc:  Table of Contents, if TOC in md_extensions
+def reencode_html(input_html):
     """
-    if md_extensions is None or not isinstance(md_extensions, list):
-        md_extensions = MD_EXTENSIONS
+    Embeds all images from an html file to base64
+    """
+    fbuffer = StringIO()
+    my_regex = re.compile('.*(<img.+src=\")(.+?)"', re.IGNORECASE)
+    for line in input_html:
+        try:
+            item = re.finditer(my_regex, line).next().group(2)
+            fbuffer.write(line.replace(item, to_b64(item)))
+        except StopIteration:
+            fbuffer.write(line)
+    fbuffer.seek(0)
+    content = fbuffer.read()
+    fbuffer.close()
+    return content
 
-    md_content = markdown.Markdown(extensions=['markdown.extensions.%s' % k
-                                               for k in md_extensions])
-    html = md_content.convert(md_fileobject.read().decode('utf-8'))
-    if any(['toc' in k for k in md_extensions]):
-        if 'meta' in md_extensions:
-            return (html, md_content.Meta, md_content.toc)
+
+class MD2Html(object):
+
+    """
+    Main class object
+    """
+
+    def __init__(self):
+        self.working_dir = os.getcwd()
+#                     os.path.dirname(os.path.abspath(__file__))
+        self.html_template = HTML_TEMPLATE
+        self.md_extensions = MD_EXTENSIONS
+
+    def main(self, **kwargs):
+        """ Renders an html from jinja2 templates filled with content converted
+            from markdown source
+            Mandatory arguments:
+            - md_fileobject: markdown input file object (passed to get_html)
+
+            Optional arguments:
+
+            - html_template:  Template for jinja2
+            - output_file:    Writtable output file descriptor object
+            - css_files:      List of CSS files to be applied to the HTML
+            - favicon_file:   Optional favicon file
+            - background_img: Optional background image file
+            - md_extensions:  List of extensions for markdown module
+            - working_dir:    Directory where /layout and /css are found
+        """
+
+        self.html_template = kwargs.get('html_template', self.html_template)
+        self.working_dir = kwargs.get('working_dir', self.working_dir)
+        self.md_extensions = kwargs.get('md_extensions', self.md_extensions)
+
+        css_files = kwargs.get('css_files', [])
+        output_file_descr = kwargs.get('output_file', None)
+        favicon_file = kwargs.get('favicon', None)
+        background_img = kwargs.get('background_img', None)
+        logo_img = kwargs.get('logo', None)
+
+        loader = jinja2.FileSystemLoader('{}/layout'.format(self.working_dir))
+        environment = jinja2.Environment(loader=loader, trim_blocks=True)
+        environment.globals['include_raw'] = \
+            lambda filename: jinja2.Markup(loader.get_source(environment,
+                                                             filename)[0]) \
+            if os.path.exists(filename) else ''
+
+        environment.globals['gen_author_list'] = \
+            lambda authors, emails: [''] if len(authors) != len(emails) else \
+            [u'<a href="mailto:{}">{}</a>'.format(emails[i], author)
+             for (i, author) in enumerate(authors)]
+
+        environment.globals['gen_copyright_list'] = \
+            lambda copyright: [u'<a href={1}>{0}</a>'.format(*item.split(','))
+                               for item in copyright]
+
+        environment.globals['css_compress'] = self.css_compress
+
+        environment.globals['to_base64'] = \
+            lambda image: to_b64(image[0])
+
+        template = environment.get_template(self.html_template)
+        content, metadata, toc = self.get_html(kwargs.get('md_fileobject'))
+
+        if 'logo' in metadata:  # input argument overrides MD's metadata
+            logo_img = logo_img or metadata['logo'][0]
+
+        if 'favicon' in metadata:  # input argument overrides MD's metadata
+            favicon_file = favicon_file or metadata['favicon'][0]
+
+        if 'background' in metadata:
+            background_img = background_img or metadata['background'][0]
+
+        markdown_date = ', '.join(metadata['date']) if 'date' in metadata \
+            else time.strptime('%d-%B-%Y')
+
+        fbuffer = StringIO()
+        fbuffer.write(template.render(css_lines=self.css_compress(css_files),
+                                      content=content,
+                                      metadata=metadata,
+                                      toc=toc,
+                                      favicon=to_b64(favicon_file),
+                                      logo=to_b64(logo_img),
+                                      background=to_b64(background_img),
+                                      date=markdown_date).encode('utf-8'))
+        fbuffer.seek(0)
+        if output_file_descr:
+            output_file_descr.write(reencode_html(fbuffer))
         else:
-            return (html, None, md_content.toc)
-    else:
-        if 'meta' in md_extensions:
-            return (html, md_content.Meta, None)
+            print(reencode_html(fbuffer))
+        fbuffer.close()
+        return
+
+    def css_compress(self, css_files):
+        """ Reads file by file and returns the compressed version of the CSS
+        css_files: list of files, located in /css subfolder
+        """
+        def css_wrapper(css_files):
+            """ Wrapper for css_compress handling with non-existing files """
+            for item in css_files:
+                try:
+                    with open('{}/{}'.format(self.working_dir, item),
+                              'r') as css_file:
+                        yield compress(css_file.read())
+                except IOError:
+                    yield ''
+
+        return ''.join(css_wrapper(['css/%s' % css for css in css_files]))
+
+    def get_html(self, md_fileobject):
+        """ Converts markdown syntax to html
+             - md_fileobject: markdown input file object
+
+            Returns:
+            - html:    HTML encoded string
+            - md.Meta: Metadata as found in md header (if any)
+            - md.toc:  Table of Contents, if TOC in md_extensions
+        """
+        md_content = markdown.Markdown(extensions=['markdown.extensions.%s' % k
+                                                   for k in self.md_extensions]
+                                       )
+        html = md_content.convert(md_fileobject.read().decode('utf-8'))
+        if any(['toc' in k for k in self.md_extensions]):
+            if 'meta' in self.md_extensions:
+                return (html, md_content.Meta, md_content.toc)
+            else:
+                return (html, None, md_content.toc)
         else:
-            return (html, None, None)
+            if 'meta' in self.md_extensions:
+                return (html, md_content.Meta, None)
+            else:
+                return (html, None, None)
 
 if __name__ == "__main__":
+
+    MD_TO_HTML = MD2Html()
     PARSER = argparse.ArgumentParser(description='Markdown to HTML converter',
                                      formatter_class=argparse.
                                      RawTextHelpFormatter)
@@ -197,9 +211,9 @@ if __name__ == "__main__":
                         type=str, help='CSS files')
 
     PARSER.add_argument('-T', '--template',
-                        dest='html_template', default=HTML_TEMPLATE,
-                        type=str, help='HTML template for Jinja2, '
-                                       'defaults to {}'.format(HTML_TEMPLATE))
+                        dest='html_template', default=MD_TO_HTML.html_template,
+                        type=str, help='HTML template for Jinja2, defaults to '
+                                       '{}'.format(MD_TO_HTML.html_template))
 
     PARSER.add_argument('-F', '--favicon',
                         dest='favicon',
@@ -214,9 +228,10 @@ if __name__ == "__main__":
                         type=str, help='Logo image (PNG/BMP/JPG)')
 
     PARSER.add_argument('-M', '--extensions', metavar='MARKDOWN_EXTENSION',
-                        dest='md_extensions', nargs='+', default=MD_EXTENSIONS,
-                        type=str, help='Extensions from markdown module, '
-                                       'defaults to: {}'.format(MD_EXTENSIONS))
+                        dest='md_extensions', nargs='+', type=str,
+                        default=MD_TO_HTML.md_extensions,
+                        help='Extensions from markdown module, defaults to '
+                             '{}'.format(MD_TO_HTML.md_extensions))
 
     PARSER.add_argument('md_fileobject', default=sys.stdin,
                         type=argparse.FileType('r'), metavar='input_file',
@@ -227,5 +242,4 @@ if __name__ == "__main__":
                         help='HTML output file, defaults to stdout')
 
     ARGS = PARSER.parse_args()
-
-    render_html(**vars(ARGS))
+    MD_TO_HTML.main(**vars(ARGS))
--- test/test_md2html.py
+++ test/test_md2html.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 """
-*pysmscmon* - SMSC monitoring **test functions**
+md2html - test functions
 """
 
 import unittest
@@ -13,34 +13,25 @@ from cStringIO import StringIO
 class TestMd2html(unittest.TestCase):
     """ Set of test functions for md2html module """
 
-    def test_csscompress(self):
-        """ Test function for css_compress """
-        self.assertListEqual([k for k in md2html.css_compress([])], [])
-        css_files = ['../md2html/css/lanyon.css',
-                     '../md2html/css/codehilite.css']
-        css_comp1 = md2html.css_compress(css_files)
-        self.assertIsNotNone(''.join([css for css in css_comp1]))
+    def __init__(self, *args, **kwargs):
+        super(TestMd2html, self).__init__(*args, **kwargs)
+        self.test = md2html.MD2Html()
 
-        css_comp1 = md2html.css_compress(css_files)
-        css_comp2 = md2html.css_compress(css_files + ['nonexisting.css'])
-        self.assertEqual(''.join([css for css in css_comp1]),
-                         ''.join([css for css in css_comp2]))
-
-    def test_to_b64image(self):
-        """ Test function for to_b64_image """
+    def test_to_b64(self):
+        """ Test function for to_b64() """
         local_image = "../md2html/layout/images/Acme-logo.png"
         remote_image = "http://upload.wikimedia.org/wikipedia/"\
                        "commons/7/7e/Acme-logo.png"
-        self.assertIsNotNone(md2html.to_b64_image(local_image))
-        self.assertIsNotNone(md2html.to_b64_image(remote_image))
-        self.assertEqual(md2html.to_b64_image(local_image),
-                         md2html.to_b64_image(remote_image))
-        self.assertIsNone(md2html.to_b64_image('nonexisting_file.png'))
-        self.assertIsNone(md2html.to_b64_image('http://localhost/nofile.jpg'))
-        self.assertIsNone(md2html.to_b64_image('http://wrong.url/img.png'))
+        self.assertIsNotNone(md2html.to_b64(local_image))
+        self.assertIsNotNone(md2html.to_b64(remote_image))
+        self.assertEqual(md2html.to_b64(local_image),
+                         md2html.to_b64(remote_image))
+        self.assertIsNone(md2html.to_b64('nonexisting_file.png'))
+        self.assertIsNone(md2html.to_b64('http://localhost/nofile.jpg'))
+        self.assertIsNone(md2html.to_b64('http://wrong.url/img.png'))
 
     def test_reencodehtml(self):
-        """ Test function for reencode_html """
+        """ Test function for reencode_html() """
         html = ['<html><body><h1>TITLE</h1><a>Hello World!</a>',
                 '</body></html>']
         self.assertEqual(md2html.reencode_html(html),
@@ -50,8 +41,21 @@ class TestMd2html(unittest.TestCase):
                 '</body></html>']
         self.assertIn('data:image/png;base64,', md2html.reencode_html(html))
 
-    def test_md2html(self):
-        """ Test function for md2html """
+    def test_csscompress(self):
+        """ Test function for MD2Html.css_compress() """
+        self.assertListEqual([k for k in self.test.css_compress([])], [])
+        css_files = ['../md2html/css/lanyon.css',
+                     '../md2html/css/codehilite.css']
+        css_comp1 = self.test.css_compress(css_files)
+        self.assertIsNotNone(''.join([css for css in css_comp1]))
+
+        css_comp1 = self.test.css_compress(css_files)
+        css_comp2 = self.test.css_compress(css_files + ['nonexisting.css'])
+        self.assertEqual(''.join([css for css in css_comp1]),
+                         ''.join([css for css in css_comp2]))
+
+    def test_gethtml(self):
+        """ Test function for MD2Html.get_html() """
         md_string = '#Title\n##This is my markdown file!\n![Alt](!'\
                     'https://duckduckgo.com/assets/badges/logo_square.64.png'\
                     ')\n`End of file`'
@@ -60,7 +64,8 @@ class TestMd2html(unittest.TestCase):
         fbuffer.write(md_string)
 
         fbuffer.seek(0)
-        html = md2html.md2html(fbuffer, md_extensions=[])
+        self.test.md_extensions = []
+        html = self.test.get_html(fbuffer)
         self.assertMultiLineEqual(html[0],
                                   '<h1>Title</h1>\n'
                                   '<h2>This is my markdown file!</h2>\n'
@@ -71,25 +76,28 @@ class TestMd2html(unittest.TestCase):
         self.assertIsNone(html[2])
 
         fbuffer.seek(0)
-        html = md2html.md2html(fbuffer)
+        self.test.md_extensions = md2html.MD_EXTENSIONS
+        html = self.test.get_html(fbuffer)
         self.assertDictEqual(html[1], {})  # empty metadata
         fbuffer.close()
 
         fbuffer = StringIO()
         fbuffer.write('metadata: metadata example\n%s' % md_string)
         fbuffer.seek(0)
-        html = md2html.md2html(fbuffer)
+        html = self.test.get_html(fbuffer)
         map(self.assertIsNotNone, html)
         self.assertDictEqual(html[1],
                              {u'metadata': [u'metadata example']})
         fbuffer.close()
 
-    def test_renderhtml(self):
-        """ End to end test of overall module by running render_html """
+    def test_main(self):
+        """ End to end test of overall module by running MD2Html.main()
+        """
         tmp_file = StringIO()
         with open('test.md', 'r') as md_fileobject:
-            md2html.render_html(md_fileobject=md_fileobject,
-                                output_file=tmp_file)
+            self.test.main(md_fileobject=md_fileobject,
+                           output_file=tmp_file,
+                           working_dir='../md2html')
         with open('test.html', 'r') as htmlfile:
             that = htmlfile.read()
         tmp_file.seek(0)
